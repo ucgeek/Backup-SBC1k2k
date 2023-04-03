94b444388d2ac4dc13fc0d3f20aac5e2ab747470
ucgeek <39198233+ucgeek@users.noreply.github.com>
2023-04-04 07:54:19 +1200

    Changes to TLS settings
    Line 66 -  [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12

--- Backup-SBC1k2k.ps1
+++ Backup-SBC1k2k.ps1
@@ -11,19 +11,16 @@
     Download: https://github.com/ucgeek/Backup-SBC1k2k
     
     Creates backups for Ribbon (previously Sonus) SBC1000 & SBC2000 devices using PowerShell.
-
 .RUN INSTRUCTIONS 
     Update the settings below to suit your environment:
     $Gateways is a key/value pair - key=name, value=sbcFQDN e.g. "SBC01" = "sbc01.domain.com";
     $BackupFolder = path to save backup ending with \ e.g. "C:\SfBBackup\SBC\"
     $BackupsDaysToKeep = numbers of days to keep backup files
     $Username / $Password = a user with 'rest' level access created in the SBC - https://support.sonus.net/display/UXDOC61/Permissions+Overview
-
-
     You can automate the script using a scheduled task on a server that has access to the SBC IP. Use PowerShell.exe. with the following arguments: '-command "& '<PATH>\Backup-SBC1k2k.ps1'"
-
 .NOTES
-    v1.0 - Initial release       
+    v1.0 - Initial release
+    v1.1 - TLS changes    
 #>
 #endregion INFO
 
@@ -66,8 +63,7 @@ foreach ($gateway in $Gateways.Keys)
 
     #Authenticate to SBC
     [System.Net.ServicePointManager]::ServerCertificateValidationCallback = { $true }
-    #[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-    #[Net.ServicePointManager]::SecurityProtocol =  [Net.SecurityProtocolType]::Tls12 -bor [Net.SecurityProtocolType]::Tls11 -bor [Net.SecurityProtocolType]::Tls
+    [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
     $LoginUrl = "https://" + $IP + "/rest/login"
     $LoginCredentials = "Username=" + $Username + "&Password=" + $Password
 
@@ -77,5 +73,4 @@ foreach ($gateway in $Gateways.Keys)
     $args = ""
     $BackupUrl = "https://" + $IP + "/rest/system?action=backup"
     Invoke-RestMethod -Uri $BackupUrl -Method POST -Body $args -WebSession $ps -OutFile $BackupPath
-
 }
